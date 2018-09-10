9e067d24ca9a948fb55e2d50bad64d48140c5212
Timothy Hoang <timh1203@gmail.com>
2018-09-10 16:06:03 -0400

    Add router and route

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,32 @@
-import React, { Component } from 'react';
-
-import SavedList from './Movies/SavedList';
-import MovieList from './Movies/MovieList';
-import Movie from './Movies/Movie';
+import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
+import SavedList from './Movies/SavedList'
+import MovieList from './Movies/MovieList'
+import Movie from './Movies/Movie'
 
 export default class App extends Component {
   constructor() {
-    super();
+    super()
     this.state = {
       savedList: []
-    };
+    }
   }
 
   addToSavedList = movie => {
-    const savedList = this.state.savedList;
-    savedList.push(movie);
-    this.setState({ savedList });
-  };
+    const savedList = this.state.savedList
+    savedList.push(movie)
+    this.setState({ savedList })
+  }
 
   render() {
     return (
       <div>
         <SavedList list={this.state.savedList} />
         <div>Replace this Div with your Routes</div>
+
+      <Route path="/" component={MovieList} />
+      <Route path="/movies/:id" component={Movie} />
       </div>
-    );
+    )
   }
 }
