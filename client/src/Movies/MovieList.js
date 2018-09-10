4850bb1efe57a68617e82c23349908df3d08bf43
Timothy Hoang <timh1203@gmail.com>
2018-09-10 16:16:32 -0400

    Add link in movielist component

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom'
 
 export default class MovieList extends Component {
   constructor(props) {
@@ -23,9 +24,11 @@ export default class MovieList extends Component {
   render() {
     return (
       <div className="movie-list">
-        {this.state.movies.map(movie => (
-          <MovieDetails key={movie.id} movie={movie} />
-        ))}
+          {this.state.movies.map(movie => (
+            <Link key={movie.id} to={`/movies/${movie.id}`}>
+              <MovieDetails key={movie.id} movie={movie} />
+            </Link>
+          ))}
       </div>
     );
   }
