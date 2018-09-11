f08420384e11d78bbe3ada0cc882244c2206543a
Timothy Hoang <timh1203@gmail.com>
2018-09-11 16:05:42 -0400

    Add refactor to moviecard component

--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 const MovieCard = props => {
-  return;
+  return (
+    <Fragment>
+      <div className="movie-card">
+        <h2>{props.movie.title}</h2>
+        <div className="movie-director">
+          Director: <em>{props.movie.director}</em>
+        </div>
+        <div className="movie-metascore">
+          Metascore: <strong>{props.movie.metascore}</strong>
+        </div>
+        <h3>Actors</h3>
+
+        {props.movie.stars.map(star => (
+          <div key={star} className="movie-star">
+            {star}
+          </div>
+        ))}
+      </div>
+      <div className="save-button" onClick={() => this.saveMovie()}>Save</div>
+    </Fragment>
+  )
 };
 
 export default MovieCard;
