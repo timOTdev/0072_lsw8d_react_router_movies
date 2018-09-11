3aba359e8609f3d5b0f8c3f4647eba4679cc90aa
Timothy Hoang <timh1203@gmail.com>
2018-09-11 16:33:33 -0400

    Add links to savedlist links

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -7,7 +7,9 @@ export default class SavedList extends Component {
       <div className="saved-list">
         <h3>Saved Movies:</h3>
         {this.props.list.map(movie => (
-          <span key={movie.id} className="saved-movie">{movie.title}</span>
+          <Link to={`/movies/${movie.id}`}>
+            <span key={movie.id} className="saved-movie">{movie.title}</span>
+          </Link>
         ))}
         <Link to="/"><div className="home-button">Home</div></Link>
       </div>
